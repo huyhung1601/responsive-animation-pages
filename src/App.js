fa821282a730e0ecea299fa7186b1c3d637b72fa
Henry Nguyen <huyhung1601@gmail.com>
2021-08-04 16:46:40 +1000

    hover drop menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Home/>
     </div>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './styles.scss'
+const Home = () => {
+    return (
+        <div className="home">
+            <nav>
+                <div className="logo">CodingNepal</div>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li>
+                        <a href="#">Features</a>
+                        <ul>
+                            <li><a href="#">Pages</a></li>
+                            <li><a href="#">Elements</a></li>
+                            <li><a href="#">Icons</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                        <ul>
+                            <li><a href="#">Web Design</a></li>
+                            <li><a href="#">App Design</a></li>
+                            <li>
+                                <a href="#">More ...</a>
+                                <ul>
+                                    <li><a href="">Submenu-1</a></li>
+                                    <li><a href="">Submenu-2</a></li>
+                                    <li><a href="">Submenu-3</a></li>
+                                    
+                                </ul>
+
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            
+        </div>
+    )
+}
+
+export default Home
